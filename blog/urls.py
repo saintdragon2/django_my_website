d45c55713b2fb3ba24fa7093272cf68f99b94345
SungYong Lee <saintdragon2@gmail.com>
2019-02-03 15:50:25 +0900

    blog post list (index)

--- blog/urls.py
+++ blog/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from . import views
+
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all()
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
