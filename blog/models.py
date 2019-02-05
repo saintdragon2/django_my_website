a1db48b24cb0109946ad22efc1fce559701497e9
SungYong Lee <saintdragon2@gmail.com>
2019-02-05 22:33:06 +0900

    head_image on blog-post

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
+
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
 
--- my_site_prj/urls.py
+++ my_site_prj/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
