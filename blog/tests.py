4ab4ad07c2009eae0eb8855f0da4564ca3789c4e
SungYong Lee <saintdragon2@gmail.com>
2019-02-09 23:24:46 +0900

    post list test done

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='smith', password='nopassword')
+
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title
+
+        self.assertEqual(title.text, 'Blog')
+
+        navbar = soup.find('div', id='navbar')
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
+
+        self.assertEqual(Post.objects.count(), 0)
+        self.assertIn('아직 게시물이 없습니다.', soup.body.text)
+
+        post_000 = Post.objects.create(
+            title='The first post',
+            content='Hello World. We are the world.',
+            created=timezone.now(),
+            author=self.author_000,
+        )
+
+        self.assertGreater(Post.objects.count(), 0)
+
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        body = soup.body
+        self.assertNotIn('아직 게시물이 없습니다.', body.text)
+        self.assertIn(post_000.title, body.text)
+
