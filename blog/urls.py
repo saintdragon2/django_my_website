deee22cd319e7f97dba27fe2a0652a7a44af83ac
SungYong Lee <saintdragon2@gmail.com>
2019-02-09 18:35:58 +0900

    Post Detail: initialized

--- blog/urls.py
+++ blog/urls.py
@@ -18,5 +18,6 @@ from . import views
 
 
 urlpatterns = [
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class PostList(ListView):
@@ -9,6 +9,20 @@ class PostList(ListView):
     def get_queryset(self):
         return Post.objects.order_by('-created')
 
+
+class PostDetail(DetailView):
+    model = Post
+
+# def post_detail(request, pk):
+#     blog_post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'blog_post': blog_post,
+#         }
+#     )
 
 # def index(request):
 #     posts = Post.objects.all()
