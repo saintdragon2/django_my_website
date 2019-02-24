6d8f6254a00d7c4c2742559c805e7aab24850344
SungYong Lee <saintdragon2@gmail.com>
2019-02-24 22:59:05 +0900

    pretty google login

--- my_site_prj/urls.py
+++ my_site_prj/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
