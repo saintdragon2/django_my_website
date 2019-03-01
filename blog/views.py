8269e3acbff85d47f2cba578d3a89f9a5ae1636f
SungYong Lee <saintdragon2@gmail.com>
2019-03-01 20:02:40 +0900

    delete_comment: back to FBV: test done

--- blog/views.py
+++ blog/views.py
@@ -114,33 +114,27 @@ def new_comment(request, pk):
         return redirect('/blog/')
 
 
-class CommentDelete(DeleteView):
-    model = Comment
+def delete_comment(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    post = comment.post
+    if request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url() + '#comment-list')
+    else:
+        raise PermissionError('Comment 삭제 권한이 없습니다.')
 
-    def get_object(self, queryset=None):
-        comment = super(CommentDelete, self).get_object()
-        if comment.author != self.request.user:
-            raise PermissionError('Comment 삭제 권한이 없습니다.')
-        return comment
-
-    def get_success_url(self):
-        post = self.get_object().post
-        return post.get_absolute_url() + '#comment-list'
-
-
-# def delete_comment(request, pk):
-#     comment = Comment.objects.get(pk=pk)
-#     post = comment.post
-#     if request.user == comment.author:
-#         comment.delete()
-#         return redirect(post.get_absolute_url() + '#comment-list')
-#     else:
-#         return redirect('/blog/')
-
-
-
-
-
+# class CommentDelete(DeleteView):
+#     model = Comment
+#
+#     def get_object(self, queryset=None):
+#         comment = super(CommentDelete, self).get_object()
+#         if comment.author != self.request.user:
+#             raise PermissionError('Comment 삭제 권한이 없습니다.')
+#         return comment
+#
+#     def get_success_url(self):
+#         post = self.get_object().post
+#         return post.get_absolute_url() + '#comment-list'
 
 # def post_detail(request, pk):
 #     blog_post = Post.objects.get(pk=pk)
