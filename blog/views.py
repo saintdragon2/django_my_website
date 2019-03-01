fc5912a76d679b763f872a3a352730a1e7a378c6
SungYong Lee <saintdragon2@gmail.com>
2019-03-02 00:44:15 +0900

    pagination

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostList(ListView):
     model = Post
-
-    def get_queryset(self):
-        return Post.objects.order_by('-created')
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data(**kwargs)
