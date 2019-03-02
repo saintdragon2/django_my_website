5239872b89cbef677cdb77002bd9370b8b65f0ee
SungYong Lee <saintdragon2@gmail.com>
2019-03-02 23:27:43 +0900

    about me page initialized

--- basecamp/urls.py
+++ basecamp/urls.py
@@ -13,18 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from . import views
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
-    path('markdownx/', include('markdownx.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('', include('basecamp.urls')),
+    path('about_me/', views.about_me),
+    path('', views.index),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- my_site_prj/settings.py
+++ my_site_prj/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +48,9 @@ INSTALLED_APPS = [
 
     'markdownx',
     'crispy_forms',
+    
+    'blog',
+    'basecamp',
 ]
 
 MIDDLEWARE = [
